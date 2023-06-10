d62454204e29e68c4e468d301bf1f8b461d9d6bc
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-10 20:58:37 -0300

    Consumir o State na aplicação

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import ResumenPedido from './views/ResumenPedido';
 
 // importar state de context
 import FirebaseState from './context/firebase/firebaseState';
+import PedidosState from './context/pedidos/pedidosState';
 
 const Stack = createStackNavigator();
 
@@ -21,66 +22,68 @@ const App = () => {
   return (
     <>
       <FirebaseState>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: {
-                backgroundColor: '#FFDA00',
-              },
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-              headerTitleAlign: 'center',
-            }}>
-            <Stack.Screen
-              name="NuevaOrden"
-              component={NuevaOrden}
-              options={{
-                title: 'Nueva Orden',
-              }}
-            />
+        <PedidosState>
+          <NavigationContainer>
+            <Stack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: '#FFDA00',
+                },
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+                headerTitleAlign: 'center',
+              }}>
+              <Stack.Screen
+                name="NuevaOrden"
+                component={NuevaOrden}
+                options={{
+                  title: 'Nueva Orden',
+                }}
+              />
 
-            <Stack.Screen
-              name="Menu"
-              component={Menu}
-              options={{
-                title: 'Menu',
-              }}
-            />
+              <Stack.Screen
+                name="Menu"
+                component={Menu}
+                options={{
+                  title: 'Menu',
+                }}
+              />
 
-            <Stack.Screen
-              name="DetallePlatillo"
-              component={DetallePlatillo}
-              options={{
-                title: 'Detalle Platillo',
-              }}
-            />
+              <Stack.Screen
+                name="DetallePlatillo"
+                component={DetallePlatillo}
+                options={{
+                  title: 'Detalle Platillo',
+                }}
+              />
 
-            <Stack.Screen
-              name="FormularioPlatillo"
-              component={FormularioPlatillo}
-              options={{
-                title: 'Formulario Platillo',
-              }}
-            />
+              <Stack.Screen
+                name="FormularioPlatillo"
+                component={FormularioPlatillo}
+                options={{
+                  title: 'Formulario Platillo',
+                }}
+              />
 
-            <Stack.Screen
-              name="ResumenPedido"
-              component={ResumenPedido}
-              options={{
-                title: 'Resumen Pedido',
-              }}
-            />
+              <Stack.Screen
+                name="ResumenPedido"
+                component={ResumenPedido}
+                options={{
+                  title: 'Resumen Pedido',
+                }}
+              />
 
-            <Stack.Screen
-              name="ProgresoPedido"
-              component={ProgresoPedido}
-              options={{
-                title: 'Progreso Pedido',
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
+              <Stack.Screen
+                name="ProgresoPedido"
+                component={ProgresoPedido}
+                options={{
+                  title: 'Progreso Pedido',
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PedidosState>
       </FirebaseState>
     </>
   );
