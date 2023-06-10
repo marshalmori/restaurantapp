608ffd2c9be9eb3f05c8ffe48735c12719aa934c
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-10 17:42:32 -0300

    Criar State, reducer e context para o firebase

--- App.js
+++ App.js
@@ -12,71 +12,76 @@ import NuevaOrden from './views/NuevaOrden';
 import ProgresoPedido from './views/ProgresoPedido';
 import ResumenPedido from './views/ResumenPedido';
 
+// importar state de context
+import FirebaseState from './context/firebase/firebaseState';
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#FFDA00',
-            },
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerTitleAlign: 'center',
-          }}>
-          <Stack.Screen
-            name="NuevaOrden"
-            component={NuevaOrden}
-            options={{
-              title: 'Nueva Orden',
-            }}
-          />
+      <FirebaseState>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#FFDA00',
+              },
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+              headerTitleAlign: 'center',
+            }}>
+            <Stack.Screen
+              name="NuevaOrden"
+              component={NuevaOrden}
+              options={{
+                title: 'Nueva Orden',
+              }}
+            />
 
-          <Stack.Screen
-            name="Menu"
-            component={Menu}
-            options={{
-              title: 'Menu',
-            }}
-          />
+            <Stack.Screen
+              name="Menu"
+              component={Menu}
+              options={{
+                title: 'Menu',
+              }}
+            />
 
-          <Stack.Screen
-            name="DetallePlatillo"
-            component={DetallePlatillo}
-            options={{
-              title: 'Detalle Platillo',
-            }}
-          />
+            <Stack.Screen
+              name="DetallePlatillo"
+              component={DetallePlatillo}
+              options={{
+                title: 'Detalle Platillo',
+              }}
+            />
 
-          <Stack.Screen
-            name="FormularioPlatillo"
-            component={FormularioPlatillo}
-            options={{
-              title: 'Formulario Platillo',
-            }}
-          />
+            <Stack.Screen
+              name="FormularioPlatillo"
+              component={FormularioPlatillo}
+              options={{
+                title: 'Formulario Platillo',
+              }}
+            />
 
-          <Stack.Screen
-            name="ResumenPedido"
-            component={ResumenPedido}
-            options={{
-              title: 'Resumen Pedido',
-            }}
-          />
+            <Stack.Screen
+              name="ResumenPedido"
+              component={ResumenPedido}
+              options={{
+                title: 'Resumen Pedido',
+              }}
+            />
 
-          <Stack.Screen
-            name="ProgresoPedido"
-            component={ProgresoPedido}
-            options={{
-              title: 'Progreso Pedido',
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Screen
+              name="ProgresoPedido"
+              component={ProgresoPedido}
+              options={{
+                title: 'Progreso Pedido',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FirebaseState>
     </>
   );
 };
