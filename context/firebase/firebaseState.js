abcf3115a86f0e917b8452e8c7ac378f8d2bd742
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-16 08:58:38 -0300

    Configuração inicial do Menu

--- context/firebase/firebaseState.js
+++ context/firebase/firebaseState.js
@@ -3,17 +3,28 @@ import firebase from '../../firebase';
 import FirebaseReducer from './firebaseReducer';
 import FirebaseContext from './firebaseContext';
 
+import {OBTENER_PRODUCTOS} from '../../types';
+
 const FirebaseState = props => {
   //Crear state inicial
   const initialState = {
     menu: [],
+    error: false,
   };
 
   //useReducer con dispatch para ejecutar las funciones
   const [state, dispatch] = useReducer(FirebaseReducer, initialState);
 
+  // Función que se ejecuta para traer los productos
+  const obtenerProductos = () => {
+    dispatch({
+      type: OBTENER_PRODUCTOS,
+    });
+  };
+
   return (
-    <FirebaseContext.Provider value={{menu: state.menu, firebase}}>
+    <FirebaseContext.Provider
+      value={{menu: state.menu, firebase, obtenerProductos}}>
       {props.children}
     </FirebaseContext.Provider>
   );
--- views/Menu.js
+++ views/Menu.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import FirebaseContext from '../context/firebase/firebaseContext';
 
 const Menu = () => {
+  // Context de Firebase
+  const {obtenerProductos} = useContext(FirebaseContext);
+
+  useEffect(() => {
+    obtenerProductos();
+  }, []);
+
   return <Text>Menu</Text>;
 };
 
